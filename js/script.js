5953e657ded516566bad60b06ee6e75bcf863103
Lexie Brown <goldsworthy@protonmail.com>
2018-07-24 00:59:41 -0400

    reverted typing to original

--- js/script.js
+++ js/script.js
@@ -1,57 +1,26 @@
 var i = 0;
-var txt = ''; /* The text */
+var txt = 'Hello traveler.. Welcome to my little corner of the web..'; /* The text */
 var arrow = ' &#x25BF;';
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
-
-  console.log(window.location.pathname);
-  switch(window.location.pathname) {
-    case '/':
-      txt = 'Hello traveler.. Welcome to my little corner of the web..';
-      typeHello();
-      break;
-    case '/#stats.html':
-      txt = 'Stats';
-      typeStats();
-      break;
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    if (i == 16) {
+      document.getElementById("demo").innerHTML += '<br><br>';
+      i++;
+    }
+  } else { 
+    document.getElementById("arrow").innerHTML += arrow;
+    document.getElementById("arrow").className = "blinking";
+    blinker();
   }
 }
 
-function typeHello() {
-    if (i < txt.length) {
-      document.getElementById("demo").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-      if (i == 16) {
-        document.getElementById("demo").innerHTML += '<br><br>';
-        i++;
-      }
-    } else { 
-      document.getElementById("arrow").innerHTML += arrow;
-      document.getElementById("arrow").className = "blinking";
-      blinker();
-    }
-}
-
-function typeStats() {
-    if (i < txt.length) {
-      document.getElementById("demo").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-      if (i == 16) {
-        document.getElementById("demo").innerHTML += '<br><br>';
-        i++;
-      }
-    } else { 
-      document.getElementById("arrow").innerHTML += arrow;
-      document.getElementById("arrow").className = "blinking";
-      blinker();
-    }
-}
-
 function blinker() {
-	$('.blinking').fadeOut(1000);
-	$('.blinking').fadeIn(1000);
+  $('.blinking').fadeOut(1000);
+  $('.blinking').fadeIn(1000);
 }
 setInterval(blinker, 2000);
